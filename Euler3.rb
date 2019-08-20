d5e0b9099fd9651af5ec9349799f542b072c0c99
James Dunn <james.dunn@protolif.com>
2019-08-20 07:05:48 -0400

    using built-in Ruby features for a one-liner answer

--- Euler3.rb
+++ Euler3.rb
@@ -5,12 +5,15 @@
 # Load our handy math library
 $LOAD_PATH << './lib'
 require 'HandyMath'
+require 'Prime'
 
 class Euler3
   using HandyMath
   def initialize
     # assign a variable called answer
-    answer = 0
+    # Ruby's Prime class includes a factorization method that returns a matrix of factors
+    # #max#max returns the greatest value in an array matrix
+    answer = Prime.instance.prime_division(600851475143).max.max
     # output the answer to the command line
     puts "Answer: #{answer}"
   end
